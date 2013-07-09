4cbd42c41f3d6a02a5734e3521dd0ab89e4b7991
kpolaiah <kpolaiah@heresy.in>
2013-07-09 19:14:32 +0530

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoDeploy::Application.config.secret_token = '270c69453335ea0bf676a01f0e21734fac52a419c4ddc6a6a588553cdd5fd246a22003a9bb5c612136051a704313daf0b7f3d0d204027a20588e2d29478c192d'
